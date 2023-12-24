4e7b1d7141f9dac6782ba099a4eff28b661d0095
Hamdi Adan <hhhaji013@gmail.com>
2023-12-24 23:49:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = 62 * 1;
+const random = Math.ceil (Math.random()*100);
+
+const num3 = 8;
+const num4 = 2;
+const mod = 8 / 2;
+
+const max =Math.max(18,19,20);
